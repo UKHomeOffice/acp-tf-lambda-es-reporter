9ee7b4e0e680fdfa52a529be085e4fde8085154c
Willem Veerman <willem.veerman@digital.homeoffice.gov.uk>
2021-08-31 17:59:15 +0100

    increment lambda version

--- lambda.tf
+++ lambda.tf
@@ -1,21 +1,7 @@
-# resource null_resource dummy_trigger {
-#   triggers = {
-#     timestamp = timestamp()
-#   }
-# }
-
-locals {
-  uuid = uuid()
-}
-
 data "archive_file" "lambda_zip" {
   type        = "zip"
-  output_path = "/tmp/lambda_zip_${local.uuid}.zip"
+  output_path = "${path.module}/tmp/lambda_zip.zip"
   source_dir  = "${path.module}/lambda/"
-  # depends_on = [
-  # # Make sure archive is created in apply stage
-  #   null_resource.dummy_trigger
-  # ]
 }
 
 data "aws_ssm_parameter" "elasticsearch_password" {
